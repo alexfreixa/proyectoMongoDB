78e49e246ee56c830999b09f20a04453bae0cabd
Alex Freixa <alexfreixa18@gmail.com>
2019-04-10 16:10:43 +0200

    Commit Inicial amb concesionari

---
+++ models/concesionario.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var ConcesionarioSchema = new Schema(
+  {
+    nombre_concesionario: {type: String, required: true, max: 100},
+    marca_concesionario: {type: String, required: true, max: 100},
+    localizacion_concesionario: {type: String, required: true, max: 100},
+    fecha_apertura_concesionario: {type: Date}
+  }
+);
+
+// Virtual for author's full name
+ConcesionarioSchema
+.virtual('nombre')
+.get(function () {
+  return this.nombre_concesionario;
+});
+
+ConcesionarioSchema
+.virtual('marca')
+.get(function () {
+  return this.marca_concesionario;
+});
+
+ConcesionarioSchema
+.virtual('localizacion')
+.get(function () {
+  return this.localizacion_concesionario;
+});
+
+ConcesionarioSchema
+.virtual('fecha_apertura')
+.get(function () {
+  return this.fecha_apertura_concesionario ? moment(this.fecha_apertura_concesionario).format('YYYY-MM-DD') : 'Sin información';
+});
+
+// Virtual for concesionario's URL
+ConcesionarioSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/concesionario/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Concesionario', ConcesionarioSchema);
